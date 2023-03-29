dbbdce83a87a9f4c51356120e4ac29ca31b152a2
umaicode <park.seungchan99@gmail.com>
2023-03-29 22:32:40 +0900

    use array

--- Test_chap3_MatricClass.cpp
+++ Test_chap3_MatricClass.cpp
@@ -20,6 +20,68 @@ private:
 	int* Term;// a[i][j] = i * cols + j
 };
 
+Matrix::Matrix(int row, int col) {
+	this->rows = row;
+	this->cols = col;
+	this->Term = new int[row * col];
+}
+
+int Matrix::GetData() {
+	for (int i = 0; i < this->rows * this->cols; i++) {
+		this->Term[i] = rand() % 10;
+	}
+	return 1;
+}
+
+int Matrix::Display() {
+	for (int i = 0; i < this->rows; i++) {
+		for (int j = 0; j < this->cols; j++) {
+			cout << this->Term[i * this->cols + j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	return 1;
+}
+
+Matrix Matrix::Add(Matrix b) {
+	if (this->rows != b.rows || this->cols != b.cols) {
+		cout << "¾ÈµÊ" << endl;
+		return Matrix(0,0);
+	}
+
+	Matrix a2(this->rows, this->cols);
+	for (int i = 0; i < this->rows; i++) {
+		for (int j = 0; j < this->cols; j++) {
+			a2.Term[i * this->cols + j] = this->Term[i * this->cols + j] + b.Term[i * this->cols + j];
+		}
+	}
+	return a2;
+}
+
+Matrix Matrix::Multiply(Matrix b) {
+	Matrix c(this->rows, b.cols);
+	for (int i = 0; i < this->rows; i++) {
+		for (int j = 0; j < b.cols; j++) {
+			int sum = 0;
+			for (int k = 0; k < this->cols; k++) {
+				sum += this->Term[i * this->cols + k] * b.Term[k * b.cols + j];
+			}
+			c.Term[i * c.cols + j] = sum;
+		}
+	}
+	return c;
+}
+
+Matrix Matrix::Transpose() {
+	Matrix d(this->cols, this->rows);
+	for (int i = 0; i < this->cols; i++) {
+		for (int j = 0; j < this->rows; j++) {
+			d.Term[i * d.cols + j] = this->Term[j * this->cols + i];
+		}
+	}
+	return d;
+}
 
 int main()
 {
